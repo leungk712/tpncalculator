33a5ac2291c40634f79c0c24cd06a7ff2b68c1fa
Kevin Leung <42705941+leungk712@users.noreply.github.com>
2019-08-10 15:57:22 -0600

    Update JS from ES5 to ES6

--- tpn.js
+++ tpn.js
@@ -11,10 +11,7 @@ const calculate = document.querySelector("#calculate").addEventListener("click",
 
 // RESTART BUTTON
 
-const reset = document.querySelector("#reset").addEventListener("click", () => {
-  location.reload();
-});
-
+const reset = document.querySelector("#reset").addEventListener("click", () => location.reload());
 
 // FUNCTION CALLS
 
@@ -22,10 +19,12 @@ dextrose();
 protein();
 lipids();
 time();
-buttonlb();
+pounds();
 calories();
 
-function tpn(){
+// TPN
+
+const tpn = () => {
   const gramsDex = document.getElementById("dextroseInput").value;
   const gir = document.getElementById("gir");
   const tpnHours = document.querySelector("#hoursInput").value;
@@ -34,7 +33,7 @@ function tpn(){
   const caloriesPro = document.querySelector("#protein-kg");
   const proteinGrams = document.getElementById("proteinInput").value;
   const total = dextrose() + protein() + lipids() + 150;
-  const lbValue = buttonlb() /  2.2;
+  const lbValue = pounds() /  2.2;
   const girTotal = (gramsDex / lbValue / time()).toFixed(2);
   fluidml.textContent = total;
   fluidliters.textContent = (total / 1000).toFixed(2);
@@ -43,53 +42,53 @@ function tpn(){
   caloriesText.textContent = calories();
   caloriesKg.textContent = (calories() / lbValue).toFixed(1);
   caloriesPro.textContent = (proteinGrams / lbValue).toFixed(1);
-}
+};
 
 // DEXTROSE
 
-function dextrose(){
+const dextrose = () => {
     const dextroseGrams = document.getElementById("dextroseInput").value;
     const dextroseCalc = Math.ceil((dextroseGrams * 100) / 70);
     return dextroseCalc
-}
+};
 
 // PROTEIN
 
-function protein(){
+const protein = () => {
     const proteinGrams = document.getElementById("proteinInput").value;
     const proteinCalc = Math.ceil((proteinGrams * 100) / 10);
     return proteinCalc
-}
+};
 
 // LIPIDS
 
-function lipids(){
+const lipids = () => {
     const lipidsGrams = document.getElementById("lipidsInput").value;
     const lipidsCalc = Math.ceil((lipidsGrams * 100) / 20);
     return lipidsCalc
-}
+};
 
 // TIME
 
-function time(){
+const time = () => {
   const tpnHours = document.querySelector("#hoursInput").value;
   const minutes = (tpnHours  * 60) / 1000;
   return minutes
-}
+};
 
-// BUTTON POUNDS
+// POUNDS
 
-function buttonlb() {
+const pounds = () => {
    const lbValue = document.querySelector("#weight").value;
    return lbValue
-}
+}; 
 
 // CALORIES
 
-function calories(){
+const calories = () => {
   const dexCal = document.getElementById("dextroseInput").value * 3.4;
   const proCal = document.getElementById("proteinInput").value * 4;
   const lipCal = document.getElementById("lipidsInput").value * 10;
   const macroTotals = dexCal + proCal + lipCal;
   return macroTotals
-}
+};
